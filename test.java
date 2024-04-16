f2bdd5cbfa33d785fc0b53ece6ce9c7afb25931a
nhvu2311 <maxvu190@gmail.com>
2024-04-16 21:58:23 +0700

    test1

---
+++ test.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VongLapVaReNhanhTest {
+
+    @Test
+    public void testSoNguyenTo1() {
+        int n = 2;
+        boolean expectedPrime = true;
+
+        boolean actualPrime = VongLapVaReNhanh.isPrime(n);
+
+        assertEquals(expectedPrime, actualPrime);
+    }
+
+    @Test
+    public void testSoNguyenToKhong1() {
+        int n = 15;
+        boolean expectedPrime = false;
+
+        boolean actualPrime = VongLapVaReNhanh.isPrime(n);
+
+        assertEquals(expectedPrime, actualPrime);
+    }
+
+    @Test
+    public void testInSoNguyenToTu1DenN() {
+        int n = 10;
+        String expectedOutput = "2 3 5 7 ";
+
+        String actualOutput = VongLapVaReNhanh.printPrimeNumbersFrom1ToN(n);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
